32e50bf5543063ffeeae91711796c2f328201e71
Atahan Uz <atahan.uz@std.yildiz.edu.tr>
2023-08-04 01:38:24 +0300

    2023-08-04 01:38:24, atahan

--- scraper.py
+++ scraper.py
@@ -61,7 +61,7 @@ def worker(task):
     increment = float(increment)
 
     # merge argument, price and increment in a string.
-    result = f'KODQQ= {name}\nNAME= {fullname}\nPRICE= {price}\nCHANGE= {increment}\n\n'
+    result = f'KODQ= {name}\nNAME= {fullname}\nPRICE= {price}\nCHANGE= {increment}\n\n'
 
     finaltext += result
 
