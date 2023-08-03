ecc687a2939a43251151ccf032e581317a2f29a0
Atahan Uz <atahan.uz@std.yildiz.edu.tr>
2023-08-03 15:37:37 +0300

    tefas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 import requests
 from bs4 import BeautifulSoup
@@ -8,7 +9,6 @@ import sys
 def main(*arguments):
     finaltext = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n\n"
     baseurl="https://www.tefas.gov.tr/FonAnaliz.aspx?FonKod="
-    #add= input("Fon Kodu Giriniz: ")
 
     if arguments is None:
         arguments= sys.argv[1:]
@@ -16,7 +16,6 @@ def main(*arguments):
     arguments= list(arguments)
 
     for add in arguments:
-
         url= baseurl+add
 
         r=requests.get(url)
@@ -25,32 +24,30 @@ def main(*arguments):
 
         #first 1000 characters of the string
         all_text = all_text[500:800]
-        name= re.findall(f'{"Analiz"}(.*?){"Son"}', all_text)[0]
-        #print(name)
 
+        name_search = re.search('Analiz(.*?)Son', all_text)
+        name = name_search.group(1) if name_search else 'Unknown'
 
+        increment_search = re.search('\)%\s*(.*?)Pay', all_text)
+        increment = increment_search.group(1).replace(',', '.') if increment_search else '0.0'
 
+        price_search = re.search('t \(TL\)\s*(.*?)Günlük', all_text)
+        price = price_search.group(1).replace(',', '.') if price_search else '0.0'
 
-       # print(all_text)
-        increment = re.findall(f'{re.escape(")%")}(.*?){"Pay"}', all_text)[0]
-        price= re.findall(f'{re.escape("t (TL)")}(.*?){"Günlük"}', all_text)[0]
-
-
-
-
-        increment = increment.replace(',','.')
-        price = price.replace(',','.')
         #convert to float
         price = float(price)
         increment = float(increment)
 
-
         #merge argument, price and increment in a string.
         result = f'FON= {name}\nKOD= {add}\nPRICE= {price}\nCHANGE= {increment}\n\n'
-        #print(result)
         finaltext += result
-    #print(finaltext)
+
     return finaltext
 
+
+
 if __name__ == '__main__':
-    main()
+    start_time = time.time()
+    text=main("MAC", "AFT", "IPJ", "TCD", "TKF", "AEH")
+    print("--- %s seconds ---" % (time.time() - start_time))
+    print(text)
