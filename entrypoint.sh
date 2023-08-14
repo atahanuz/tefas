213ba5267208eb3dea46b23f23d9176a07bb40b8
Atahan Uz <atahan.uz@std.yildiz.edu.tr>
2023-08-15 01:26:12 +0300

    2023-08-15 01:26:12,

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
-git clone https://github.com/atahanuz/tefas.git tempfolder
+git clone https://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@github.com/atahanuz/tefas.git tempfolder
+
 rsync -a tempfolder/ .
 rm -rf tempfolder
 python3 mailer.py
